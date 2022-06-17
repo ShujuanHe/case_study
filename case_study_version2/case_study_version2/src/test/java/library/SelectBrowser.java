4d69a40311d36a357fff9f4c59ebc316cff17388
Shujuan He <shujuanhe1@gmail.com>
2022-06-17 10:47:03 -0600

    having problem with driver, captcha

--- case_study_version2/case_study_version2/src/test/java/library/SelectBrowser.java
+++ case_study_version2/case_study_version2/src/test/java/library/SelectBrowser.java
@@ -10,18 +10,16 @@ public class SelectBrowser {
 
     static WebDriver driver;
 
-    public static WebDriver StartBrowser(String browserName){
+    public static WebDriver StartBrowser(String browserName) {
         // ---If the browser is Firefox----
-        if (browserName.equalsIgnoreCase("Firefox"))
-        {
+        if (browserName.equalsIgnoreCase("Firefox")) {
 
-            System.setProperty("webdriver.firefox.marionette", "C:\\Users\\shuju\\Automation Testing\\SBAReview (1)\\SBAReview\\src\\test\\resources\\geckodriver.exe");
+            System.setProperty("webdriver.firefox.marionette", "C:\\Users\\shuju\\Automation Testing\\Case study_httpswww.alexandnova.com\\case_study_version2\\case_study_version2\\case_study_version2\\src\\test\\resources\\geckodriver.exe");
 
             driver = new FirefoxDriver();
         }
         //---- If the browser is Chrome--
-        else if(browserName.equalsIgnoreCase("Chrome"))
-        {
+        else if (browserName.equalsIgnoreCase("Chrome")) {
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\shuju\\Automation Testing\\SBAReview (1)\\SBAReview\\src\\test\\resources\\chromedriver.exe");
 
             driver = new ChromeDriver();
@@ -30,9 +28,8 @@ public class SelectBrowser {
 
         // ----If the browser is  EdgeIE----
 
-        else if(browserName.equalsIgnoreCase("EdgeExplore"))
-        {
-            System.setProperty("webdriver.edge.driver", "C:\\Users\\shuju\\Automation Testing\\SBAReview (1)\\SBAReview\\src\\test\\resources\\msedgedriver.exe");
+        else if (browserName.equalsIgnoreCase("EdgeExplore")) {
+            System.setProperty("webdriver.edge.driver", "C:\\Users\\shuju\\Automation Testing\\Case study_httpswww.alexandnova.com\\case_study_version2\\case_study_version2\\case_study_version2\\src\\test\\resources\\msedgedriver.exe");
             EdgeOptions options = new EdgeOptions();
             driver = new EdgeDriver(options);
 
